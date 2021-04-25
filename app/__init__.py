d9ae5ceae0931fdc16a7db25259e13a7a1e07149
mahan-noor <ismahanabey@gmail.com>
2021-04-25 09:08:46 +0300

    create a user class

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+def create_app(config_name):
+    app = Flask(__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
+    # Setting up configuration
+    app.config.from_object(DevConfig)
+    app.config.from_pyfile("config.py")
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
-
+    # Initializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
 
