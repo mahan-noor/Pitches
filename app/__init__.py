6d07aeacc08b0bacd72bcf8459b3cdd8a9321386
mahan-noor <ismahanabey@gmail.com>
2021-04-25 15:26:37 +0300

    import flask_script and create app instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from config import config_options
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -14,4 +16,14 @@ def create_app(config_name):
     # Initializing Flask Extensions
     bootstrap.init_app(app)
     db.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
 
--- config.py
+++ config.py
@@ -4,3 +4,18 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lenovo:1234@localhost/watchlist'
+
+
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
