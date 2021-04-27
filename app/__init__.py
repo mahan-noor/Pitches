4c362110a93591c44e103f6377571bdd3917fb0f
mahan-noor <ismahanabey@gmail.com>
2021-04-27 15:05:25 +0300

    add flask-upload and update user profile for photos

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_wtf.csrf import CSRFProtect, CSRFError
 
 login_manager = LoginManager()
@@ -11,13 +12,22 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 csrf = CSRFProtect()
+photos = UploadSet('photos',IMAGES)
+
 
 def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    
 
+
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    
     # Initializing Flask Extensions
     bootstrap.init_app(app)
     db.init_app(app)
--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lenovo:1234@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 
