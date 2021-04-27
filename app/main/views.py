ad4e1cf1ae60b10f6374d1b32c35b29bace47bf4
mahan-noor <ismahanabey@gmail.com>
2021-04-27 14:22:35 +0300

    updated viewsof main by adding a profile function and create a profile file in html

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
-from flask_login import login_required 
+from flask_login import login_required, current_user
 from flask import render_template,redirect,url_for,abort
 from . import main
-from ..models import User
-from flask_login import login_required, current_user
+from ..models import User,Role,Pitch,Comment,Upvote,Downvote
+from .forms import ReviewForm,UpdateProfile
+from .. import db
 
 
 
@@ -18,5 +19,33 @@ def index():
     title = 'Home - pitchesssss '
 
     return render_template('index.html', title = title)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
 
 
--- app/models.py
+++ app/models.py
@@ -14,6 +14,8 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pitches = db.relationship('Pitch',backref = 'role',lazy="dynamic")
     comment = db.relationship('Comment',backref = 'role',lazy="dynamic")
