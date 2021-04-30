1b2da5cccea96435aed85911112fbe2e0ff88401
mahan-noor <ismahanabey@gmail.com>
2021-04-30 07:27:39 +0300

    add change to start and change name from updated to update.html

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='6cce09d62500516652af3ee8f584e27e'
 export MAIL_USERNAME='mahannoor850@gmail.com'
-export MAIL_PASSWORD='mahannoor850'
+export MAIL_PASSWORD='12345'
 
 python3.8 manage.py server
